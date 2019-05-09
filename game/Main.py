7a076e88c2267e9b4e0fab3fd19ebbce1ea0cab9
MatheusOliveiraAmorim <matheus.oliveira.amorim@hotmail.com>
2019-05-09 20:00:55 -0300

    jogador com plataformas, scroll da tela bugado

--- game/Main.py
+++ game/Main.py
@@ -1,24 +1,111 @@
 import pygame as pg
 import random
-#import jogo-config
 #from jogo-config import *
+#from sprite import *
 
 
+
+#Arrumar Import
+#Arrumar Scroll da tela
+
+#3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+#                                                                                            2
+#   Arquivo: jogo-config.py                                                                  2
+#                                                                                            2
+#222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
 
 #opções e variáves imutáveis do jogo
 TITULO = "Corsários do Mar de Areia"
-LARGURA = 360
-ALTURA = 480
-FPS = 30
+LARGURA = 1280
+ALTURA = 720
+FPS = 60
 
+#Propriedades de jogador
+JOGADOR_ACEL = 0.5
+JOGADOR_FRIC = -0.12
+JOGADOR_GRAV = 0.8
+
+#Propriedades de plataformas
+
+LISTA_PLATAFORMA = [(0, ALTURA - 40, LARGURA, 40),
+                    (LARGURA/2 - 50, ALTURA * 3 / 4, 100, 20),
+                    (125, ALTURA - 350, 100, 20),
+                    (50, 90, 60, 10),
+                    (175, 100, 50, 20),
+                    (175, 100, 50, 20)]
 #Paleta de cores (VALORES EM RGB)
 BRANCO = (255,255,255)
 PRETO = (0,0,0)
 VERMELHO = (255,0,0)
 VERDE = (0,255,0)
 AZUL = (0,0,255)
+AMARELO = (255,255,0)
 
-class jogo:
+
+#222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+#                                                                                            2
+#   Arquivo: sprite.py                                                                       2
+#                                                                                            2
+#222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+vec = pg.math.Vector2 #inicializa um vetor de 2 dimensões
+
+class Jogador(pg.sprite.Sprite):
+    def __init__(self, jogo):
+        #Classe com os atributos do jogador
+        pg.sprite.Sprite.__init__(self)
+        #Passando o atributo jogo para o jogador ele toma conhecimento de todos os objetos(self) no código, assim esses podem ser usados como referencia
+        self.jogo = jogo
+        self.image = pg.Surface((30,40))
+        self.image.fill(AMARELO)
+        self.rect = self.image.get_rect()
+        self.rect.center = (ALTURA/2, LARGURA/2)
+        self.pos = vec(LARGURA/2, ALTURA/2) #utiliza o vetor de 2 posições aqui para armazenar parametros (Posição, velocidade, aceleração)
+        self.vel = vec(0,0)
+        self.acel = vec(0,0)
+
+    def pulo(self):
+        #verifica se existe plataforma abaixo antes de pular
+        self.rect.x += 1
+        colPlat = pg.sprite.spritecollide(self, self.jogo.plataformas, False)#exemplo de uso do parametro jogo para usar as plataformas para comparação na colisão
+        self.rect.x -= 1
+        if colPlat:
+            self.vel.y = -20
+
+    def update(self):
+        #Método que verifica a tecla pressionada e movimenta o jogador
+        self.acel = vec(0, JOGADOR_GRAV)
+        tecla = pg.key.get_pressed()
+        if tecla[pg.K_LEFT]:
+            self.acel.x = -JOGADOR_ACEL
+        if tecla[pg.K_RIGHT]:
+            self.acel.x = JOGADOR_ACEL
+
+        #Adiciona coeficiente de fricção a equação de movimento para estabilizar o movimento do jogador
+        self.acel.x += self.vel.x * JOGADOR_FRIC
+        #Equações de movimento
+        self.vel += self.acel
+        self.pos += self.vel + 0.5 * self.acel
+        #Para na borda da tela
+        if self.pos.x > LARGURA:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = LARGURA
+
+        self.rect.midbottom = self.pos
+
+class Plataforma(pg.sprite.Sprite):
+    def __init__(self, x, y, l, a):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((l, a))
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+#3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+class Jogo:
     def __init__(self):
         # Inicializa a tela
         pg.init()
@@ -31,6 +118,13 @@ class jogo:
     def novo(self):
         # Começa um jogo novo
         self.todos_sprites = pg.sprite.Group()
+        self.plataformas = pg.sprite.Group()
+        self.jogador = Jogador(self)
+        self.todos_sprites.add(self.jogador)
+        for plat in LISTA_PLATAFORMA:
+            p = Plataforma(*plat)
+            self.todos_sprites.add(p)
+            self.plataformas.add(p)
         self.executando()
 
     def executando(self):
@@ -47,6 +141,21 @@ class jogo:
     def update(self):
         # Loop´do jogo - atualiza a interface
         self.todos_sprites.update()
+        #Checa se o jogador colidiu com a plataforma(apenas se estiver caindo)
+        if self.jogador.vel.y > 0:
+            colPlat = pg.sprite.spritecollide(self.jogador, self.plataformas, False)
+            if colPlat:
+                self.jogador.pos.y = colPlat[0].rect.top +1
+                self.jogador.vel.y = 0
+        #se jogador chegar em certo ponto da tela
+        if self.jogador.rect.left <= ALTURA - 200:
+            self.jogador.pos.x -= abs(self.jogador.vel.x)
+            for plat in self.plataformas:
+                plat.rect.x -= abs(self.jogador. vel.x)
+        if self.jogador.rect.right <= ALTURA/4:
+            self.jogador.pos.x += abs(self.jogador.vel.x)
+            for plat in self.plataformas:
+                plat.rect.x += abs(self.jogador.vel.x)
 
     def eventos(self):
         # Loop´do jogo - captura de eventos
@@ -56,6 +165,10 @@ class jogo:
                 if self.jogando:
                     self.jogando = False
                 self.executando = False
+            #Verifica se alguma tecla foi pressionada
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_z:
+                    self.jogador.pulo()
 
     def desenhar(self):
         # Loop´do jogo - desenha a tela
@@ -69,7 +182,7 @@ class jogo:
     def mostra_tela_game_over(self):
         pass
 
-g = jogo()
+g = Jogo()
 g.mostra_tela_inicio()
 while g.rodando:
     g.novo()
--- game/jogo-config.py
+++ game/jogo-config.py
@@ -1,8 +1,8 @@
 #opções e variáves imutáveis do jogo
 TITULO = "Corsários do Mar de Areia"
-LARGURA = 360
-ALTURA = 480
-FPS = 30
+LARGURA = 480
+ALTURA = 360
+FPS = 60
 
 #Paleta de cores (VALORES EM RGB)
 BRANCO = (255,255,255)
@@ -10,3 +10,4 @@ PRETO = (0,0,0)
 VERMELHO = (255,0,0)
 VERDE = (0,255,0)
 AZUL = (0,0,255)
+AMARELO = (255,255,0)
