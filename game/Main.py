7fb2c92cba8e8638c01ba7b5348f9e49636ed1bc
MatheusOliveiraAmorim <matheus.oliveira.amorim@hotmail.com>
2019-05-10 14:37:11 -0300

    definida a dimensão da tela e novo método de scroll da camera

--- game/Main.py
+++ game/Main.py
@@ -21,27 +21,12 @@ ALTURA = 720
 FPS = 60
 
 #Propriedades de jogador
-JOGADOR_ACEL = 0.5
+JOGADOR_ACEL = 0.8
 JOGADOR_FRIC = -0.12
-JOGADOR_GRAV = 0.8
-JOGADOR_PULO = 20
-
-#Propriedades de plataformas
-
-LISTA_PLATAFORMA = [(0, ALTURA - 40, LARGURA, 40),#chão
-                    (300, 600, 100, 100),#caixa esquerda
-                    (400, 600, 100, 100), #caixa direita
-                    (0, 430, 150, 35), #plataforma canto
-                    (240, 250, 180, 35), #degrau
-                    (630, 250, 180, 35)] #plataforma porta
+JOGADOR_GRAV = 0.85
+JOGADOR_PULO = 25
 
 
-#LISTA_PLATAFORMA = [(0, ALTURA - 40, LARGURA, 40),
-                    #(LARGURA/2 - 50, ALTURA * 3 / 4, 100, 20),
-                    #(125, ALTURA - 350, 100, 20),
-                    #(50, 90, 60, 10),
-                    #(175, 100, 50, 20),
-                    #(175, 100, 50, 20)]
 #Paleta de cores (VALORES EM RGB)
 BRANCO = (255,255,255)
 PRETO = (0,0,0)
@@ -58,6 +43,7 @@ AMARELO = (255,255,0)
 #222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
 
 vec = pg.math.Vector2 #inicializa um vetor de 2 dimensões
+ipos = (554.167, 681)
 
 class Jogador(pg.sprite.Sprite):
     def __init__(self, jogo):
@@ -65,12 +51,12 @@ class Jogador(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         #Passando o atributo jogo para o jogador ele toma conhecimento de todos os objetos(self) no código, assim esses podem ser usados como referencia
         self.jogo = jogo
-        self.image = pg.Surface((30,40))
+        self.image = pg.Surface((200, 320))
         self.image.fill(AMARELO)
         self.rect = self.image.get_rect()
         self.rect.center = (ALTURA/2, LARGURA/2)
         #self.pos = vec(LARGURA/2, ALTURA/2) #utiliza o vetor de 2 posições aqui para armazenar parametros (Posição, velocidade, aceleração)
-        self.pos = vec(1000, 700) #utiliza o vetor de 2 posições aqui para armazenar parametros (Posição, velocidade, aceleração)
+        self.pos = vec(ipos) #utiliza o vetor de 2 posições aqui para armazenar parametros (Posição, velocidade, aceleração)
         self.vel = vec(0,0)
         self.acel = vec(0,0)
 
@@ -84,6 +70,7 @@ class Jogador(pg.sprite.Sprite):
 
     def update(self):
         #Método que verifica a tecla pressionada e movimenta o jogador
+        #print(self.pos)
         self.acel = vec(0, JOGADOR_GRAV)
         tecla = pg.key.get_pressed()
         if tecla[pg.K_LEFT]:
@@ -97,12 +84,34 @@ class Jogador(pg.sprite.Sprite):
         self.vel += self.acel
         self.pos += self.vel + 0.5 * self.acel
         #Para na borda da tela
-        if self.pos.x > LARGURA:
+        '''if self.pos.x > LARGURA:
             self.pos.x = 0
         if self.pos.x < 0:
-            self.pos.x = LARGURA
+            self.pos.x = LARGURA'''
 
         self.rect.midbottom = self.pos
+
+
+#222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+#                                                                                            2
+#   Arquivo: fase.py                                                                         2
+#                                                                                            2
+#222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+LISTA_PLATAFORMA_TUTO = [(-1280, ALTURA - 40, LARGURA*2, 40),#chão
+                    (-165, 349, 330, 330),#caixa esquerda
+                    (-496, 349, 330, 330), #caixa direita
+                    (-1280, 430, 360, 35), #plataforma canto
+                    (-1280, 250, 180, 35), #degrau
+                    (-1280, -1, 180, 35)] #plataforma porta
+
+
+#LISTA_PLATAFORMA = [(0, ALTURA - 40, LARGURA, 40),
+                    #(LARGURA/2 - 50, ALTURA * 3 / 4, 100, 20),
+                    #(125, ALTURA - 350, 100, 20),
+                    #(50, 90, 60, 10),
+                    #(175, 100, 50, 20),
+                    #(175, 100, 50, 20)]
 
 class Plataforma(pg.sprite.Sprite):
     def __init__(self, x, y, l, a):
@@ -112,6 +121,43 @@ class Plataforma(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+class Mapa:
+    #def __init__(self, nFase, nQuadrante, filename):
+    def __init__(self, nQuadrante):
+        #self.nFase = nFase
+        self.nQuadrante = nQuadrante
+        self.qAltura = ALTURA
+        self.qLargura = LARGURA
+        self.MapaAltura = nQuadrante * self.qAltura / 2
+        self.MapaLargura = nQuadrante * self.qLargura / 2
+        print(self.MapaAltura, self.MapaLargura)
+
+class Camera:
+    def __init__(self, cLargura, cAltura):
+        self.camera = pg.Rect(0, 0, cLargura, cAltura)
+        self.cAltura = cAltura
+        self.cLargura = cLargura
+        self.camLimAlt = Mapa(4).MapaAltura
+        self.camLimLarg = Mapa(4).MapaLargura
+
+    def apply(self, entidade):
+        return entidade.rect.move(self.camera.topleft)
+
+    def update(self, alvo):
+        x = -alvo.rect.x + int(LARGURA/2)
+        y = -alvo.rect.y + int(ALTURA/2)
+
+        #limitar câmera
+        x = min(0, x)
+        x = max(-(self.camLimLarg - LARGURA), x)
+        y = min(0, y)
+        y = max(-(self.camLimAlt - ALTURA), y)
+        print(x, y)
+        if alvo.vel.x > 0:
+            x += 300
+        self.camera = pg.Rect(x, y, self.cLargura, self.cAltura)
+
 
 #3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
 
@@ -127,14 +173,16 @@ class Jogo:
 
     def novo(self):
         # Começa um jogo novo
+        self.mapa = Mapa(4)
         self.todos_sprites = pg.sprite.Group()
         self.plataformas = pg.sprite.Group()
         self.jogador = Jogador(self)
         self.todos_sprites.add(self.jogador)
-        for plat in LISTA_PLATAFORMA:
+        for plat in LISTA_PLATAFORMA_TUTO:
             p = Plataforma(*plat)
             self.todos_sprites.add(p)
             self.plataformas.add(p)
+        self.camera = Camera(self.mapa.MapaLargura, self.mapa.MapaAltura)
         self.executando()
 
     def executando(self):
@@ -157,6 +205,8 @@ class Jogo:
             if colPlat:
                 self.jogador.pos.y = colPlat[0].rect.top +1
                 self.jogador.vel.y = 0
+
+        self.camera.update(self.jogador)
         #se jogador chegar em certo ponto da tela
         '''if self.jogador.rect.left <= ALTURA - 200:
             self.jogador.pos.x -= abs(self.jogador.vel.x)
@@ -183,7 +233,9 @@ class Jogo:
     def desenhar(self):
         # Loop´do jogo - desenha a tela
         self.janela.fill(PRETO)
-        self.todos_sprites.draw(self.janela)
+        #self.todos_sprites.draw(self.janela)
+        for sprite in self.todos_sprites:
+            self.janela.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
 
     def mostra_tela_inicio(self):
---
+++ game/fase.py
@@ -0,0 +1,4 @@
+import pygame as pg
+import random
+#from jogo-config import *
+#from sprite import *
